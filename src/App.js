a1d865c53bfb5ccfa40c601db8c7a0f95ed96bb0
Hallowfall <alirasyid10@gmail.com>
2026-06-21 19:53:58 +0700

    Fix React version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "100px",
+        fontFamily: "Arial"
+      }}
+    >
+      <h1>Faizal Daffa Pramudya-Progressive Web App</h1>
+      <h3>Pemrograman Web Lanjut</h3>
+      <p>Implementasi Progressive Web App (PWA) menggunakan React.</p>
     </div>
   );
 }
